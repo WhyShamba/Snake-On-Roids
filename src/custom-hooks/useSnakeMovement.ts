7ef4f84fb1bb2f591d8f1067830fe3d484fb6b8a
Blagoj <blagoj.petrov5@gmail.com>
2021-04-07 01:58:54 +0200

    perf: Optimize app and code
    Optimizing code by using react memo and useCallback
    Fix bug for wrong directioning in cistom movement hook
    
    Closes #3

--- src/custom-hooks/useSnakeMovement.ts
+++ src/custom-hooks/useSnakeMovement.ts
@@ -1,14 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { CellData, DIRECTION } from '../containers/Game';
 import { SingleLinkedList } from '../utils/SingleLinkedList';
 import { getOppositeDirection } from '../utils/snake/snake-coordination';
 
-export const useSnakeMovement = (
-  initialDirection: DIRECTION,
-  snake: SingleLinkedList<CellData>
-) => {
-  const [direction, setDirection] = useState<DIRECTION>(initialDirection);
+export const useSnakeMovement = (snakeHeadDirection: DIRECTION) => {
+  const [direction, setDirection] = useState<DIRECTION>(snakeHeadDirection);
   const snakeCellsSizeRef = useRef<number>(1);
+  const snakeHeadDirectionRef = useRef<DIRECTION>(snakeHeadDirection);
 
   /* eslint-disable */
   useEffect(() => {
@@ -32,12 +30,17 @@ export const useSnakeMovement = (
     };
   }, []);
 
+  useEffect(() => {
+    snakeHeadDirectionRef.current = snakeHeadDirection;
+  }, [snakeHeadDirection]);
+
   //   Validates and sets direction
   const _setDirection = (_direction: DIRECTION, withCheck = true) => {
-    // Handles the case where if snake length is 2, and snake is moving to left, and user decides/misclicks to go opposite -> right, then the game will end and that should not happen
+    // Handles the case where if snake.current length is 2, and snake.current is moving to left, and user decides/misclicks to go opposite -> right, then the game will end and that should not happen
     if (
       snakeCellsSizeRef.current > 1 &&
-      _direction === getOppositeDirection(snake.head!.data!.direction) &&
+      _direction === getOppositeDirection(snakeHeadDirectionRef.current) &&
+      _direction !== snakeHeadDirectionRef.current &&
       withCheck
     )
       return;
