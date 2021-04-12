b95b41612bb59e861f70c6b3b875b35e33dc758b
Blagoj <blagoj.petrov5@gmail.com>
2021-04-12 16:33:09 +0200

    Create and fix custom hooks methods

--- src/custom-hooks/useCountdown.ts
+++ src/custom-hooks/useCountdown.ts
@@ -8,28 +8,41 @@ export const useCountdown = (
   const [count, setCount] = useState(startImmedietly ? initialCount : 0);
   const callBackRef = useRef<typeof onCountdownComplete>();
   const startRef = useRef(startImmedietly);
+  const manualCountEnabled = useRef(false);
 
   useEffect(() => {
     callBackRef.current = onCountdownComplete;
   }, [onCountdownComplete]);
 
   useEffect(() => {
-    if (count > 0) {
-      const interval = setInterval(() => {
-        setCount(count - 1);
-      }, 1000);
+    if (manualCountEnabled.current) {
+      if (count === 0 && callBackRef.current) {
+        callBackRef.current();
+      }
+    } else {
+      if (count > 0) {
+        const interval = setInterval(() => {
+          setCount(count - 1);
+        }, 1000);
 
-      return () => {
-        clearInterval(interval);
-      };
-    } else if (callBackRef.current && startRef.current) {
-      callBackRef.current();
+        return () => {
+          clearInterval(interval);
+        };
+      } else if (callBackRef.current && startRef.current) {
+        callBackRef.current();
+      }
     }
   }, [count]);
 
   const resetCount = (count?: number) => {
     startRef.current = true;
+    manualCountEnabled.current = false;
     setCount(count ? count : initialCount);
+  };
+
+  const _setCount = (_count: number) => {
+    manualCountEnabled.current = true;
+    setCount(_count);
   };
 
   const cancelCountdown = () => setCount(0);
@@ -39,5 +52,43 @@ export const useCountdown = (
     resetCount,
     startCount: resetCount, // same as reset count but with diff naming convention
     cancelCountdown,
+    setCount: _setCount,
+  };
+};
+
+export const useCountdownInfinete = (
+  onTick: (...args: any) => any,
+  startImmedietly: boolean = true
+) => {
+  const callBackRef = useRef<typeof onTick>();
+  const [start, setStart] = useState(startImmedietly);
+
+  useEffect(() => {
+    callBackRef.current = onTick;
+  }, [onTick]);
+
+  useEffect(() => {
+    if (start) {
+      const interval = setInterval(() => {
+        if (callBackRef.current) callBackRef.current();
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [start]);
+
+  const startTicks = (count?: number) => {
+    setStart(true);
+  };
+
+  const stopTicks = () => {
+    setStart(false);
+  };
+
+  return {
+    startTicks,
+    stopTicks,
   };
 };
