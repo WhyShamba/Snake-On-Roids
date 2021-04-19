f60f4c0a09dfa17b69c219f0fadcd0ec15dd53f4
Blagoj <blagoj.petrov5@gmail.com>
2021-04-19 04:32:51 +0200

    Fix the firebase warning

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/analytics';
 
 export const appConfig = {
   apiKey: 'AIzaSyDmNFu8BNPEriwbcmECY_tcpwyHuucDTds',
